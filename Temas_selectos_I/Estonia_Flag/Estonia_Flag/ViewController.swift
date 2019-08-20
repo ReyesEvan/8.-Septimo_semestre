156eb2e8fa33621b01f8eb573589130330058fd9
UDLAP19 <udlap19@MacBook-IOS-Lab-19.local>
2019-08-20 18:07:40 -0500

    First three projects Swift

---
+++ Temas_selectos_I/Estonia_Flag/Estonia_Flag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Estonia_Flag
+//
+//  Created by UDLAP19 on 8/16/19.
+//  Copyright © 2019 UDLAP19. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Temas_selectos_I/Interface/Interface/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Interface
+//
+//  Created by UDLAP19 on 8/19/19.
+//  Copyright © 2019 UDLAP19. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
