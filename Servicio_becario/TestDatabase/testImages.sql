a27e6100b9e23d52755b99f377328935d5f6f57c
AndresReyesE <andresreyes.e@outlook.com>
2019-08-23 16:11:39 -0500

    Creation of a local db for testing Scolarship Service project

---
+++ Servicio_becario/TestDatabase/testImages.sql
@@ -0,0 +1,5 @@
+    INSERT INTO image (name, img)
+        VALUES ('beautiful image', lo_import('/home/reyes/8.-Septimo_semestre/Servicio_becario/TestDatabase/resources/toph.jpeg'));
+
+-- \lo_import '/home/reyes/8.-Septimo_semestre/Servicio_becario/TestDatabase/resources/caminadora.jpg' "caminadora image"
+
